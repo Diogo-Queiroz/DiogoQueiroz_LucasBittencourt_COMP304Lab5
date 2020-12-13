ddae03babebcd3e994339aa6f83685ed45f89ecf
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2020-12-13 12:42:02 -0500

    landmark class and init method

--- app/src/main/java/com/diogo/diogoqueiroz_lucasbittencourt_comp304lab5/MainActivity.java
+++ app/src/main/java/com/diogo/diogoqueiroz_lucasbittencourt_comp304lab5/MainActivity.java
@@ -6,14 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity
 {
-
+    private ArrayList<Landmark> landmarks;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InitList();
 
         this.getSupportActionBar().setTitle("Places of Toronto");
 
@@ -27,6 +30,14 @@ public class MainActivity extends AppCompatActivity
 
     private void InitList()
     {
+        Landmark duplex = new Landmark(
+                "Duplex Avenue", "Type 1", "411 Duplex Avenue, M4R1V2",
+                "43.667138","-79.3610495");
+        Landmark eastdale = new Landmark(
+                "Eastdale Avenue", "Type 2", "90 Eastdale Avenue, M4C5A2",
+                "43.667138","-78.65644");
+        landmarks.add(duplex);
+        landmarks.add(eastdale);
         // todo Create list of POI, need to implement the Class for it as well.
     }
 }

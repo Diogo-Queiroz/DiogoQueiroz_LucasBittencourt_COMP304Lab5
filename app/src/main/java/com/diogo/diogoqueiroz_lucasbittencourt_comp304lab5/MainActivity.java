5bf31d7c21f2d686f64bce2626695de719cd5b31
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2020-12-13 02:47:09 -0500

    start of the project. using RecyclerView to show the types of places. App Title and Icon is also displayed.

--- app/src/main/java/com/diogo/diogoqueiroz_lucasbittencourt_comp304lab5/MainActivity.java
+++ app/src/main/java/com/diogo/diogoqueiroz_lucasbittencourt_comp304lab5/MainActivity.java
@@ -1,6 +1,8 @@
 package com.diogo.diogoqueiroz_lucasbittencourt_comp304lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getSupportActionBar().setTitle("Places of Toronto");
+
+        RecyclerView listOfTypesView = findViewById(R.id.list_of_types);
+        listOfTypesView.setLayoutManager(new LinearLayoutManager(this));
+
+        String[] types = getResources().getStringArray(R.array.landmark_types);
+        TypesAdapter typesAdapter = new TypesAdapter(getApplicationContext(), types, "MainActivity");
+        listOfTypesView.setAdapter(typesAdapter);
+    }
+
+    private void InitList()
+    {
+        // todo Create list of POI, need to implement the Class for it as well.
     }
 }
---
+++ app/src/main/java/com/diogo/diogoqueiroz_lucasbittencourt_comp304lab5/TypesAdapter.java
@@ -0,0 +1,60 @@
+package com.diogo.diogoqueiroz_lucasbittencourt_comp304lab5;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TypesAdapter extends RecyclerView.Adapter<TypesAdapter.MyViewHolder>
+{
+    private Context context;
+    private String[] types;
+    private String activity;
+    private SharedPreferences sharedPreferences;
+
+    public TypesAdapter(Context context, String[] types, String activity)
+    {
+        this.context = context;
+        this.types = types;
+        this.activity = activity;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView type;
+
+        public MyViewHolder(View itemView)
+        {
+            super(itemView);
+            type = itemView.findViewById(R.id.recycler_item_type);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View listItem = new View(context);
+        listItem = inflater.inflate(R.layout.recycler_item_types, parent, false);
+        MyViewHolder viewHolder = new MyViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position)
+    {
+        holder.type.setText(types[position]);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return types.length;
+    }
+}
